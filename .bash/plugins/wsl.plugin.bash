5dae84c802f6f435a343faa05cd159a5904166fa
Bingzhang Dai <bingzhangdai@users.noreply.github.com>
2021-04-14 22:28:23 +0800

    Fix bug in wsl plugin in some situtations where command_not_found_handle is not defined (#4)

--- .bash/plugins/wsl.plugin.bash
+++ .bash/plugins/wsl.plugin.bash
@@ -4,21 +4,28 @@ if _is_in_wsl; then
 
     # invoke Windows exe when there is not Linux one with the same name
     # https://github.com/microsoft/WSL/issues/2003#issuecomment-297792622
-    eval "$(echo "orig_command_not_found_handle()"; declare -f command_not_found_handle | tail -n +2)"
+    if declare -F command_not_found_handle > /dev/null; then
+        eval "$(echo "orig_command_not_found_handle()"; declare -f command_not_found_handle | tail -n +2)"
+    else
+        function orig_command_not_found_handle() {
+            if [ -x /usr/lib/command-not-found ]; then
+                /usr/lib/command-not-found -- "$1";
+                return $?;
+            else
+                if [ -x /usr/share/command-not-found/command-not-found ]; then
+                    /usr/share/command-not-found/command-not-found -- "$1";
+                    return $?;
+                else
+                    printf "%s: command not found\n" "$1" 1>&2;
+                    return 127;
+                fi;
+            fi
+        }
+    fi
     command_not_found_handle() {
         if cmd.exe /c "(where $1 || (help $1 |findstr /V Try)) >nul 2>nul && ($* || exit 0)"; then
             return $?
         else
-            # if [ -x /usr/lib/command-not-found ]; then
-            #     /usr/lib/command-not-found -- "$1"
-            #     return $?
-            # elif [ -x /usr/share/command-not-found/command-not-found ]; then
-            #     /usr/share/command-not-found/command-not-found -- "$1"
-            #     return $?
-            # else
-            #     printf "%s: command not found\n" "$1" >&2
-            #     return 127
-            # fi
             orig_command_not_found_handle $*
         fi
     }
