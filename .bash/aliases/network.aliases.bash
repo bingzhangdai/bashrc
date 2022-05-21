494e34f5547bc4259ad22877bb602c8d2060e37a
Bingzhang Dai <bingzhangdai@users.noreply.github.com>
2022-05-21 23:51:14 +0800

    Minor update to fix some errors when command not found (#15)

--- .bash/aliases/network.aliases.bash
+++ .bash/aliases/network.aliases.bash
@@ -1,6 +1,6 @@
 alias speedtest="wget -O /dev/null http://speedtest.wdc01.softlayer.com/downloads/test10.zip"
 # public ip over proxy
-alias ipaddr='curl ipinfo.io/ip'
+alias ipaddr='curl -w "\n" ipinfo.io/ip'
 # real public ip
 alias digip='dig +short myip.opendns.com @resolver1.opendns.com'
 # always include the first line
