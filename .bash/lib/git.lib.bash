8b923328bdeef44f9e4df8b4fd6da797e0faa4d6
Bingzhang Dai <bingzhangdai@users.noreply.github.com>
2022-05-31 23:30:20 +0800

    Add str::join function && minor refactor (#21)
    * str::join
    * git::branch
    * bug fix of qfind and rfind
    * more powerful typeof function

---
+++ .bash/lib/git.lib.bash
@@ -0,0 +1,68 @@
+# pure bash version of git branch, even faster than git symbolic-ref --short -q HEAD
+#
+# The following options are available:
+#   -o, --output VAR        the variable to save the branch
+#
+# usage:
+#   git::branch [-o VAR]
+#
+# example:
+#   git::branch -> 'master'
+#   git::branch -o branch -> branch='master'
+function git::branch() {
+    local _head_file _head
+    local _dir="$PWD"
+    while [ -n "$_dir" ]; do
+        _head_file="$_dir/.git/HEAD"
+        if [ -f "$_dir/.git" ]; then
+            read -r _head_file < "$_dir/.git" && _head_file="$_dir/${_head_file#gitdir: }/HEAD"
+        fi
+        [ -f "$_head_file" ] && break
+        _dir="${_dir%/*}"
+    done
+
+    [ -f "$_head_file" ] || return
+    read -r _head < "$_head_file" || return
+
+    local branch=''
+    case "$_head" in
+        ref:*)
+            branch="${_head#ref: refs/heads/}"
+            ;;
+        [0-9,a-z]*)
+            # HEAD detached
+            branch="${_head:0:9}"
+            ;;
+        *)
+            false
+            return
+            ;;
+    esac
+
+    if [ "$1" = '-o' ]; then
+        printf -v "$2" '%s' "$branch"
+    else
+        echo "$branch"
+    fi
+}
+
+# # slower version
+# function git::branch() {
+#     command -v git > /dev/null || return
+#     # "git symbolic-ref --short -q HEAD" is 40% faster than "git rev-parse --abbrev-ref HEAD"
+#     local branch=$(git symbolic-ref --short HEAD 2>&1)
+#     if [[ "$branch" = *"fatal: not a git repository"* ]]; then
+#         false
+#         return
+#     fi
+#     if  [[ "$branch" = *"fatal: ref HEAD is not a symbolic ref"* ]]; then
+#         branch=$(git rev-parse --short HEAD 2> /dev/null)
+#     fi
+#     [[ -n "$branch" ]] || return
+
+#     if [ "$1" = '-o' ]; then
+#         printf -v "$2" '%s' "$branch"
+#     else
+#         echo "$branch"
+#     fi
+# }
---
+++ .bash/lib/str.lib.bash
@@ -0,0 +1,53 @@
+# string join function
+#
+# The following options are available:
+#   -d, --delimiter DELI    delimiter (defaulting to ' ') to be attached with each element
+#   -o, --output VAR        the variable to save the joined string to
+#
+# usage:
+#   str::join [-d DELI] [-o VAR] [STRING...]
+#
+# example:
+#   join 'a' 'b' 'c' -> 'a b c'
+#   join -d ':' 'a' 'b' 'c' -> 'a:b:c'
+#   join -o output_var 'a' 'b' 'c' -> output_var='a b c'
+function str::join() {
+    local deli=' ' val='' output_var=''
+    while [[ "$1" == -* ]]; do
+        case "$1" in
+            --)
+                shift
+                break
+                ;;
+            -d|--delimiter)
+                deli="$2"
+                shift 2
+                ;;
+            -o|--output)
+                output_var="$2"
+                shift 2
+                ;;
+            -h|--help)
+                echo "Usage: str::join [-d|--delimiter DELI] [-o|--output VAR] string..."
+                return 0
+                ;;
+            -*)
+                echo "Unknown option: $1" >&2
+                str::join --help >&2
+                return 1
+                ;;
+        esac
+    done
+
+    val="$1"
+    shift
+    for arg in "$@"; do
+        val+="$deli$arg"
+    done
+
+    if [ -n "$output_var" ]; then
+        printf -v "$output_var" '%s' "${val// /\ }"
+    else
+        echo $val
+    fi
+}
