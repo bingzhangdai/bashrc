8b7bb47c7ab040e732b475675f1b92c956c8056d
Bingzhang Dai <bingzhangdai@outlook.com>
2022-11-18 21:12:32 +0800

    update

--- aliases/sudo.aliases.bash
+++ aliases/sudo.aliases.bash
@@ -10,14 +10,17 @@ alias sudo='sudo '
 #     fi
 # }
 
-function sudo() {
-    # TODO: parse args
-    if [ $# -gt 1 ] && [ "$1" = 'vi' -o "$1" = 'vim' ]; then
-        # `command` will suppress shell function lookups
-        EDITOR="$1" command sudo -e "${@:2}";
-    elif [ $# -gt 1 ] && : $(which $1) && str.starts_with _ "$_DOT_BASH_BASEDIR/bin"; then
-        command sudo bash -c "PATH=\$PATH:$_DOT_BASH_BASEDIR/bin $@"
-    else
-        command sudo $@
-    fi
-}
+# function sudo() {
+#     # TODO: parse args
+#     if [ $# -gt 1 ] && [ "$1" = 'vi' -o "$1" = 'vim' ]; then
+#         # `command` will suppress shell function lookups
+#         EDITOR="$1" command sudo -e "${@:2}";
+#     elif [ $# -gt 1 ] && : $(which $1) && str.starts_with _ "$_DOT_BASH_BASEDIR/bin"; then
+#         # command sudo bash -c "PATH=\$PATH:$_DOT_BASH_BASEDIR/bin $@"
+#         local command=$(which $1)
+#         shift
+#         command sudo $command $@
+#     else
+#         command sudo $@
+#     fi
+# }
