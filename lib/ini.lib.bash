2b45876eba778b42cbccf72cc377a08bfbfe698b
Bingzhang Dai <bingzhangdai@outlook.com>
2022-12-04 17:01:28 +0800

    Add git aliases

--- lib/ini.lib.bash
+++ lib/ini.lib.bash
@@ -1,16 +1,18 @@
 function ini::filter_by_section() {
-    local _line _section
+    local _line _section _found
     while IFS=$'\n' read -r _line; do
         _line=${_line%%;*}
         _line=${_line%%#*}
         [[ -z "$_line" ]] && continue
         [[ "$_line" =~ ^\[(.*)\]$ ]] && _section=${BASH_REMATCH[1]} && continue
         [[ "$_section" == "$1" ]] && printf '%s\n' "$_line"
+        _found=true
     done
+    [[ -n "$_found" ]]
 }
 
 function ini::get_value_by_key() {
-    local _line
+    local _line _found
     while IFS=$'\n' read -r _line; do
         _line=${_line%%;*}
         _line=${_line%%#*}
@@ -21,6 +23,8 @@ function ini::get_value_by_key() {
             : "${_#"${_%%[![:space:]]*}"}"
             : "${_%"${_##*[![:space:]]}"}"
             printf '%s\n' "$_"
+            _found=true
         fi
     done
+    [[ -n "$_found" ]]
 }
