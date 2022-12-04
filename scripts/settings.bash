dc9cb9fbe91b67ec8706f8a06d1d1b983c31d65b
Bingzhang Dai <bingzhangdai@outlook.com>
2022-12-04 15:04:26 +0800

    update

--- scripts/settings.bash
+++ scripts/settings.bash
@@ -1,16 +1,16 @@
 ## prompt: user@host:/path/to/dir(git_branch)$ 
 
-# uncomment for a colored prompt, if the terminal has the capability
+# set 'yes' for a colored prompt, if the terminal has the capability
 OPT_ENABLE_COLOR=yes
 
-# uncomment to add git information to the prompt
+# set 'yes' to add git information to the prompt
 OPT_ENABLE_GIT_BRANCH=yes
 
-# uncomment to enable abbreviated working directory in your command prompt
+# set 'yes' to enable abbreviated working directory in your command prompt
 # by default the each directory will be shortened to one character
 # e.g. /path/to/current/directory -> /p/t/c/directory
 OPT_ENABLE_SHORT_PATH=yes
-# uncomment to shrink the each directory name to the shortest unambiguous prefix
+# set 'yes' to shrink the each directory name to the shortest unambiguous prefix
 OPT_UNAMBIGUOUS_SHORT_PATH=yes
 
 # hostname will become red if the battery is below `OPT_BATTER_THRESHOLD`
